191083da335c841c33a4736a2a6eb11113772bc3
JIN <parksonice@naver.com>
2023-07-11 15:05:49 +0900

    [Update] controlnetimg2img로 변경
    - 일반 controlnet에서 img2img controlnet으로 변경
    - 그에 따른 코드들도 조금 변경함.
    - 코드 주석 추가

--- diffusion/model.py
+++ diffusion/model.py
@@ -1,52 +1,34 @@
-from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
+from diffusers import StableDiffusionControlNetImg2ImgPipeline, ControlNetModel, UniPCMultistepScheduler
 import torch
-from diffusers.utils import load_image
-import cv2
-import numpy as np
-
-
-
 
 
 def available_control_net():
     controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-openpose", torch_dtype=torch.float16)
-    pipe = StableDiffusionControlNetPipeline.from_pretrained(
+    pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
         "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
     )
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.enable_model_cpu_offload()
     pipe.enable_xformers_memory_efficient_attention()
-    
+
     return pipe
 
 
-def run_control_net(prompt, images, negative_prompt, generator, num_inference_steps):
-    output = available_control_net()(
+# prompt 개수에 따라서 생기는 image의 개수가 바뀜.(prompt의 개수는 곧 batchsize이다.)
+def run_control_net(prompt,
+                    person_image,
+                    control_net_image,
+                    negative_prompt=None,
+                    num_inference_steps=20,
+                    pipe=available_control_net()):
+    if negative_prompt is None:
+        negative_prompt = ["monochrome, lowres, bad anatomy, worst quality, low quality"]
+    diffusion_images = pipe(
         prompt,
-        images,
-        negative_prompt,
-        generator = generator,
-        num_inference_steps = num_inference_steps
+        negative_prompt=negative_prompt * len(prompt),
+        image=person_image,
+        control_image=control_net_image,
+        generator=[torch.Generator(device="cuda").manual_seed(102) for _ in range(len(prompt))],
+        num_inference_steps=num_inference_steps
     )
-    return output
-    
-
-
-
-
-prompt = ", best quality, extremely detailed"
-prompt = [t + prompt for t in ["Sandra Oh", "Kim Kardashian", "rihanna", "taylor swift"]]
-generator = [torch.Generator(device="cuda").manual_seed(102) for i in range(len(prompt))]
-
-output = pipe(
-    prompt,
-    canny_image,
-    negative_prompt=["monochrome, lowres, bad anatomy, worst quality, low quality"] * len(prompt),
-    generator=generator,
-    num_inference_steps=20,
-)
-
-print(output.images[0])
-
-for i in range(4):
-    output.images[i].save(f'./images/{i+2}.png', 'png')
+    return diffusion_images
