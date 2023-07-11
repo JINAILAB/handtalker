80039535f0283f6d673ffd26953b2c0828ec8976
JinYong <parksonice@naver.com>
2023-07-11 09:15:03 +0000

    [Fix] fix allbug

--- diffusion/model.py
+++ diffusion/model.py
@@ -4,8 +4,11 @@ import torch
 
 def available_control_net():
     controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-openpose", torch_dtype=torch.float16)
+    # pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
+    #     "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
+    # )
     pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
-        "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
+        "stablediffusionapi/anything-v5", controlnet=controlnet, torch_dtype=torch.float16
     )
     pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
     pipe.enable_model_cpu_offload()
@@ -15,20 +18,28 @@ def available_control_net():
 
 
 # prompt 개수에 따라서 생기는 image의 개수가 바뀜.(prompt의 개수는 곧 batchsize이다.)
+# gudiance_scale Prompt를 얼마나 잘 따라서 만들 것인가
+# controlnet_conditioning_scale: controlnet를 얼마나 잘 살릴 것인가
+# strength: controlnet를 얼마나 잘 살릴 것인가
+# num_inference_steps: 생성된 이미지 퀄리티, Diffusion denoising steps
 def run_control_net(prompt,
                     person_image,
                     control_net_image,
                     negative_prompt=None,
-                    num_inference_steps=20,
+                    num_inference_steps=30,
                     pipe=available_control_net()):
     if negative_prompt is None:
-        negative_prompt = ["monochrome, lowres, bad anatomy, worst quality, low quality"]
+        negative_prompt = ["nsfw, stuff, monochrome, lowres, bad anatomy, worst quality, low quality"]
     diffusion_images = pipe(
         prompt,
         negative_prompt=negative_prompt * len(prompt),
         image=person_image,
         control_image=control_net_image,
+        guidance_scale=25, # 20, 7.5
+        controlnet_conditioning_scale=1.5, # 1.5. 1.3
+        strength=0.9, # 0.9
         generator=[torch.Generator(device="cuda").manual_seed(102) for _ in range(len(prompt))],
         num_inference_steps=num_inference_steps
     )
     return diffusion_images
+
--- diffusion/run.py
+++ diffusion/run.py
@@ -1,8 +1,10 @@
-from utils import get_images
-from model import run_control_net
+from utils import *
+from model import *
 import argparse
 import numpy as np
 import torch
+import cv2
+from PIL import Image
 
 parser = argparse.ArgumentParser(description='make_movie_with_controlnet')
 parser.add_argument('--folder_name', type=str,
@@ -10,11 +12,11 @@ parser.add_argument('--folder_name', type=str,
                     help='open_pose_image가 들어있는 folder의 이름')
 
 parser.add_argument('--batch_size', type=int,
-                    default='./open_pose_images',
+                    default=2,
                     help='gpu에 맞는 batchsize를 사용할 것')
 
-parser.add_argument('--batch_size', type=int,
-                    default='./open_pose_images',
+parser.add_argument('--img_size', type=tuple,
+                    default=(560, 480),
                     help='gpu에 맞는 batchsize를 사용할 것')
 
 
@@ -25,11 +27,30 @@ args = parser.parse_args()
 
 
 def main(args=args):
-    images = get_images(args.folder_name)
-
-    prompt = "best quality, extremely detailed"
     
-    return pass
+    image = get_an_image_resize('./IU.png', (560, 480))
+    pose_images = get_images_resize(args.folder_name, hw=(560, 480))
+    pose_images = split_by_batch_size(pose_images, args.batch_size)
+    for i in range(len(pose_images)):
+        output_images = run_control_net(prompt=['best quality, a cute girl with white shirts, white background']*args.batch_size,
+                    person_image=[image]*4,
+                    control_net_image=pose_images[i],
+                    negative_prompt=None,
+                    num_inference_steps=35,
+                    pipe=available_control_net())
+        for j, img in enumerate(output_images):
+            output_images.images[j].save(f'./final_images/{i}_{j}.png', 'png')
+            
+            
+        
+        
+        
+            
+            
+            
+    
+    
+    prompt = "best quality, extremely detailed"
     
     
     
