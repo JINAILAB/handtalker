27751c871e2f3a12402be07eeabe1c6e5e903a11
JinYong <parksonice@naver.com>
2023-07-13 12:17:39 +0000

    [Feat] controlnet 3methods

--- diffusion/model.py
+++ diffusion/model.py
@@ -1,45 +1,106 @@
-from diffusers import StableDiffusionControlNetImg2ImgPipeline, ControlNetModel, UniPCMultistepScheduler
+from diffusers.models import AutoencoderKL
+import diffusers
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, EulerDiscreteScheduler, StableDiffusionPipeline
 import torch
 
+class Diff_Run:
+    def __init__(self, 
+                 prompt,
+                 model_name = 'Lykon/DreamShaper',
+                 negative_prompt = "artoon, 3d, ((disfigured)), ((bad art)), ((deformed)), ((poorly drawn)), ((extra limbs)), ((b&w)), weird colors, blurry",
+                 cfg = 6,
+                 num_inference_steps = 30):
+        self.model_name = model_name
+        self.prompt = prompt
+        self.negtive_prompt = negative_prompt
+        self.cfg = cfg
+        self.num_inference_steps = num_inference_steps
+        
+        
+    def controlnet(self):
+        controlnet = ControlNetModel.from_pretrained("lllyasviel/control_v11p_sd15_openpose",
+                                                    torch_dtype=torch.float16,
+                                                    safety_checker = None,
+                                                    requires_safety_checker = False)
+        return controlnet
+    
+    
+    def text2imgpipe(self):
+        pipe = StableDiffusionPipeline.from_pretrained(
+            self.model_name, torch_dtype=torch.float16
+        )
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+        pipe.enable_model_cpu_offload()
+        pipe.enable_xformers_memory_efficient_attention()
 
-def available_control_net():
-    controlnet = ControlNetModel.from_pretrained("lllyasviel/sd-controlnet-openpose", torch_dtype=torch.float16)
-    # pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
-    #     "runwayml/stable-diffusion-v1-5", controlnet=controlnet, torch_dtype=torch.float16
-    # )
-    pipe = StableDiffusionControlNetImg2ImgPipeline.from_pretrained(
-        "stablediffusionapi/anything-v5", controlnet=controlnet, torch_dtype=torch.float16
-    )
-    pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
-    pipe.enable_model_cpu_offload()
-    pipe.enable_xformers_memory_efficient_attention()
+        return pipe
+    
+    def text2img_controlnetpipe(self):
+        pipe = StableDiffusionControlNetPipeline.from_pretrained(
+            self.model_name, controlnet=self.controlnet, torch_dtype=torch.float16
+        )
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+        pipe.enable_model_cpu_offload()
+        pipe.enable_xformers_memory_efficient_attention()
 
-    return pipe
+        return pipe
+    
+    def text2img_lorapipe(self, model_path='./jwy.safetensors'):
+        pipe = StableDiffusionPipeline.from_pretrained(
+            self.model_name, torch_dtype=torch.float16
+        )
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+        pipe.unet.load_attn_procs(model_path)
+        pipe.enable_model_cpu_offload()
+        pipe.enable_xformers_memory_efficient_attention()
+
+        return pipe
+    
+    def text2img(self,
+                pipe,
+                ):
+                
+
+        diffusion_images = pipe(
+            prompt=self.prompt,
+            negative_prompt=self.negative_prompt,
+            guidance_scale=6, # 20, 7.5 # cfg
+            generator=torch.Generator(device="cuda").manual_seed(112),
+            num_inference_steps=self.num_inference_steps
+        )
+        return diffusion_images
+
+    def text2img_contronetl(self,
+                pipe,
+                control_net_image,
+                ):
+
+        diffusion_images = pipe(
+            control_net_image,
+            prompt=self.prompt,
+            negative_prompt=self.negative_prompt,
+            guidance_scale=6, # 20, 7.5 # cfg # prompt를 얼마나 줄지 
+            controlnet_conditioning_scale=1.8, # controlnet강도 
+            generator=torch.Generator(device="cuda").manual_seed(112),
+            num_inference_steps=self.num_inference_steps
+        )
+        return diffusion_images
+
+    def text2img_lora(self,
+                pipe,
+                ):
+                
+        if negative_prompt is None:
+            negative_prompt = ["artoon, 3d, ((disfigured)), ((bad art)), ((deformed)), ((poorly drawn)), ((extra limbs)), ((b&w)), weird colors, blurry"]
+
+        diffusion_images = pipe(
+            prompt=self.prompt,
+            negative_prompt=self.negative_prompt,
+            guidance_scale=self.cfg,
+            generator=torch.Generator(device="cuda").manual_seed(112),
+            num_inference_steps=self.num_inference_steps
+        )
+        return diffusion_images
 
 
-# prompt 개수에 따라서 생기는 image의 개수가 바뀜.(prompt의 개수는 곧 batchsize이다.)
-# gudiance_scale Prompt를 얼마나 잘 따라서 만들 것인가
-# controlnet_conditioning_scale: controlnet를 얼마나 잘 살릴 것인가
-# strength: controlnet를 얼마나 잘 살릴 것인가
-# num_inference_steps: 생성된 이미지 퀄리티, Diffusion denoising steps
-def run_control_net(prompt,
-                    person_image,
-                    control_net_image,
-                    negative_prompt=None,
-                    num_inference_steps=30,
-                    pipe=available_control_net()):
-    if negative_prompt is None:
-        negative_prompt = ["nsfw, stuff, monochrome, lowres, bad anatomy, worst quality, low quality"]
-    diffusion_images = pipe(
-        prompt,
-        negative_prompt=negative_prompt * len(prompt),
-        image=person_image,
-        control_image=control_net_image,
-        guidance_scale=25, # 20, 7.5
-        controlnet_conditioning_scale=1.5, # 1.5. 1.3
-        strength=0.9, # 0.9
-        generator=[torch.Generator(device="cuda").manual_seed(102) for _ in range(len(prompt))],
-        num_inference_steps=num_inference_steps
-    )
-    return diffusion_images
 
--- diffusion/run.py
+++ diffusion/run.py
@@ -16,7 +16,7 @@ parser.add_argument('--batch_size', type=int,
                     help='gpu에 맞는 batchsize를 사용할 것')
 
 parser.add_argument('--img_size', type=tuple,
-                    default=(560, 480),
+                    default=(400, 512),
                     help='gpu에 맞는 batchsize를 사용할 것')
 
 
@@ -28,15 +28,15 @@ args = parser.parse_args()
 
 def main(args=args):
     
-    image = get_an_image_resize('./IU.png', (560, 480))
-    pose_images = get_images_resize(args.folder_name, hw=(560, 480))
+    #image = get_an_image_resize('./IU.png', args.img_size)
+    pose_images = get_images_resize(args.folder_name, args.img_size)
     pose_images = split_by_batch_size(pose_images, args.batch_size)
     for i in range(len(pose_images)):
-        output_images = run_control_net(prompt=['best quality, a cute girl with white shirts, white background']*args.batch_size,
-                    person_image=[image]*4,
+        output_images = run_control_net(prompt=['positive- delicate, (best quality:1.2), (masterpiece:1.3), realistic, 8K UHD, High definition, High quality texture, intricate details, detailed texture, finely detailed, high detail, extremely detailed cg, High quality shadow, a realistic representation of the face, beautiful detailed, (high detailed skin, skin details), Detailed beautiful delicate face, Detailed beautiful delicate eyes, a face of perfect proportion, Depth of field, Cinematic Light, Lens Flare, Ray tracing, perspective, 20s, (photorealistic:0.8), (solo:1.4), Prominent Nose, (high nose:1.0),(sharp nose:1.0), slender face, (big eyes:1.1),Glow Eyes, blush, glossy lips, perfect body, perfect breasts, (korean celebrity, korean beauty, korean actress, kpop idol, korean), a beautiful 26 years old beautiful korean woman, best ratio four finger and one thumb, (full body:1.5), (makeup:0.4), best reality, high heels, (pantyhose:1.3), long leg , (blonde colored short hair:1.4), (school uniform of korean style:1.2), (shirt + minskirt:1.4), beautiful leg, (view from front:1.5), (smile:1.3), (hand on own waist:1.2), (perfect anatomy:1.2), wind blow    ']*args.batch_size,
+                    #person_image=[image]*args.batch_size,
                     control_net_image=pose_images[i],
                     negative_prompt=None,
-                    num_inference_steps=35,
+                    num_inference_steps=30,
                     pipe=available_control_net())
         for j, img in enumerate(output_images):
             output_images.images[j].save(f'./final_images/{i}_{j}.png', 'png')
@@ -47,10 +47,6 @@ def main(args=args):
         
             
             
-            
-    
-    
-    prompt = "best quality, extremely detailed"
     
     
     
